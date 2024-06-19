2de795eaa87ed555af3de5b4414488514eb2ce95
Sneh2633 <snehalbhogawade07713@gmail.com>
2024-06-19 17:46:02 +0530

    Adding Operators

---
+++ Operators/Program.cs
@@ -0,0 +1,76 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        /*
+        Unary Operators -> ++,--
+        Arithmetic/Mathematical Operators -> +,-,/,*,%
+        Assignment Operators -> =,+=,-=,*=,/=,%=
+        Comparison Operators -> >,<,<=,>=,==,!=
+        Logical Operators-> &&,|| 
+        Bitwise Operators -> &,|,^
+        */
+
+/*
+        int a,b;
+        a=10;
+        b=++a;
+        System.Console.WriteLine($"Prefix ++,a={a}, b={b}");
+
+        int c=a++;
+        System.Console.WriteLine($"Postfix ++, a={a},c={c}");
+
+        System.Console.WriteLine("-------------------------------------");
+        int x=50,y=20;
+        System.Console.WriteLine($"Addition = {x+y}");
+        System.Console.WriteLine($"Subtraction = {x-y}");
+        System.Console.WriteLine($"Multiplication = {x*y}");
+        System.Console.WriteLine($"Division = {x/y}");
+        System.Console.WriteLine($"Modulo = {x%y}");
+
+        System.Console.WriteLine("-------------------------------------");
+        int p=10;
+        p+=5;
+        System.Console.WriteLine($"p+=5 : {p}");
+
+        p-=5;
+        System.Console.WriteLine($"p-=5 : {p}");
+
+        p*=5;
+        System.Console.WriteLine($"p*=5 : {p}");
+
+        p/=5;
+        System.Console.WriteLine($"p/=5 : {p}");
+
+        p%=5;
+        System.Console.WriteLine($"p%=5 : {p}");
+        System.Console.WriteLine("-------------------------------------");
+
+        int q=60;
+        System.Console.WriteLine($"q==50 : {q==50}");
+        System.Console.WriteLine($"q>=50 : {q>=50}");
+        System.Console.WriteLine($"q<=50 : {q<=50}");
+        System.Console.WriteLine($"q!=50 : {q!=50}");
+        System.Console.WriteLine("-------------------------------------");
+
+        bool z=true;
+        bool r=false;
+
+        System.Console.WriteLine($"z&&r : {z && r}");
+        System.Console.WriteLine($"z||r : {z || r}");
+       System.Console.WriteLine($"!z : {!z}");
+       
+
+       int n=10;
+       for(int a=0;a<5;a++){
+        n=2;
+        System.Console.WriteLine(a*n);  */
+
+        long num1=200;
+        long num2=500;
+        int total;
+        total=num1+num2;
+        System.Console.WriteLine(total);
+       }
+    }
+}
